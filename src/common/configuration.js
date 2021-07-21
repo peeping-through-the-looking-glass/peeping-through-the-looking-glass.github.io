d7fe75d54d4da3883e441fb297a05c5039cfe31a
mcabrol <martin.cabrol@free.fr>
2021-07-21 20:45:23 +0200

    Keep the build only

--- src/common/configuration.js
+++
@@ -1,8 +0,0 @@
-export const Configuration = {
-    host: location.host,
-    origin: location.protocol == "file:" ? location.origin + location.pathname : location.origin,
-    root: document.getElementById("root"),
-    userAgent: navigator.userAgent,
-};
-
-console.log(location);
--- src/common/data.js
+++
@@ -1,6 +0,0 @@
-export const Data = {
-    exhibition: require('./../../static/data/exhibition.json'),
-    general: require('./../../static/data/general.json'),
-    language: require('./../../static/data/language.json'),
-    person: require('./../../static/data/person.json'),
-};
